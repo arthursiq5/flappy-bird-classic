7176e4d9e8eaefa7ba5f0aec2ca4e75aafc8b696
Arthur Siqueira <arthur.siq5@gmail.com>
2020-08-12 00:50:35 -0300

    utilizada função nativa do JavaScript para renderizar imagem na tela

--- jogo.js
+++ jogo.js
@@ -5,3 +5,11 @@ sprites.src = './sprites.png';
 
 const canvass = document.querySelector('canvas');
 const context = canvass.getContext('2d');
+
+context.drawImage(
+    sprites,
+    0, 0, // sprite x / sprite y
+    33, 24, // tamanho do sprite original
+    10, 50,
+    33, 24 // tamanho do sprite (renderizado)
+);
