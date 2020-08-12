d0e83769ac7524ed46e4c2e9c305b5a93ff85643
Arthur Siqueira <arthur.siq5@gmail.com>
2020-08-12 00:39:46 -0300

    configurando canvas e elementos de imagem

---
+++ jogo.js
@@ -0,0 +1,7 @@
+console.log('[arthur] flappy bird');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvass = document.querySelector('canvas');
+const context = canvass.getContext('2d');
