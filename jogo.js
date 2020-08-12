d8605b8cf9b5834e55bf8028dab9d51e240c5b05
Arthur Siqueira <arthur.siq5@gmail.com>
2020-08-12 00:53:43 -0300

    chamada função de renderização a cada frame

--- jogo.js
+++ jogo.js
@@ -6,10 +6,16 @@ sprites.src = './sprites.png';
 const canvass = document.querySelector('canvas');
 const context = canvass.getContext('2d');
 
-context.drawImage(
-    sprites,
-    0, 0, // sprite x / sprite y
-    33, 24, // tamanho do sprite original
-    10, 50,
-    33, 24 // tamanho do sprite (renderizado)
-);
+
+function loop(){
+    context.drawImage(
+        sprites,
+        0, 0, // sprite x / sprite y
+        33, 24, // tamanho do sprite original
+        10, 50,
+        33, 24 // tamanho do sprite (renderizado)
+    );
+    requestAnimationFrame(loop);
+}
+
+loop();
